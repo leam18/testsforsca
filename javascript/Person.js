0ada8a3f00f8377f49f9bb9cc925273b05b6f19d
Leandro <leandro.mansilla@teracloud.io>
2020-08-05 17:38:38 -0300

    broke code

--- javascript/Person.js
+++ javascript/Person.js
@@ -2,7 +2,7 @@ asdasfs-lflkheñwejhn
 var Person = function(first, last, middle) {
     this.first = first;
     this.middle = middle;
-    this.last = last;
+    this.la{lfnskjfasjkharñkfj.ahdnf aerñiukjmst = last;
 };
 
 Person.prototype = {
